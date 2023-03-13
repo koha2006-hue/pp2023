06c28a7a61077e153106aa7e8f99242f0c6de4ba
koha2006-hue <khoadh.bi12-215@st.usth.edu.vn>
2023-03-13 11:14:39 +0700

    update input limitations, enhance functions

--- 1.studentmark.py
+++ 1.studentmark.py
@@ -2,47 +2,79 @@
 students={}
 courses={}
 #functions to input student details:
-def student_input():
-    student_num = int(input("Enter the number of students:"))
-    for i in range(student_num):
-        student_id=input("Enter the id of student: ")
-        student_name=input("Enter the name of student: ")
-        student_DoB=input ("Enter the date of student: ")
-        students[student_id]={'name':student_name, 'DoB':student_DoB, 'mark':{} }
+def student_input(): #if student_num is not integer or less than 0, return error message
+    student_num=input("Enter the number of students: ")
+    if not student_num.isdigit():
+        print ("The number of students must be positive integer!")
+        student_input()
+    elif int(student_num) == 0:
+        return
+    elif student_num.isdigit():
+        student_num=int(student_num)
+        for i in range(student_num):
+            student_id=input("Enter the id of student: ")
+            student_name=input("Enter the name of student:")
+            student_DoB=input("Enter the date of birth of student(DD/MM/YY):")
+            students[student_id]={'name':student_name, 'DoB':student_DoB, 'mark':{}}
 #function to input course details:
 def course_input():
-    course_num=int(input("Enter the number of courses: "))
-    for i in range(course_num):
-        course_id=input("Enter the id of course: ")
-        course_name=input("Enter the name of course:")
-        courses[course_id]={'name':course_name, 'students':{}}
+    course_num=input("Enter the number of courses: ")
+    if not course_num.isdigit():
+        print ("The number of courses must be positive integer!")
+        course_input()
+    elif int(course_num) == 0:
+        return
+    elif course_num.isdigit():
+        course_num=int(course_num)
+        for i in range(course_num):
+            course_id=input("Enter the id of course: ")
+            course_name=input("Enter the name of course:")
+            courses[course_id]={'name':course_name, 'students':{}}
 #function to input mark for student in selected course:
 def mark_input():
-    course_id=input("Enter the course id: ")
+    course_list()
+    if len(courses)==0:
+        print("No course found")
+        return
+    course_id=input("Enter the course id to enter the mark: ")
     if course_id not in courses:
         print("Course id not found")
         return
     for student_id in students:
-        mark=input("Enter the mark of ID " + student_id + " in this course: ")
+        mark=float(input("Enter"+ course_id+ "mark of ID " + student_id + ":"))
         students[student_id]['mark'][course_id]= mark
         courses[course_id]['students'][student_id]=mark
 #function to list all courses:
 def course_list():
+    if len(courses)==0:
+        print("No course found")
+        return
     print("The list of courses: ")
     for course_id in courses:
         course_name= courses[course_id]['name']
         print(course_id, course_name)
 #function to list all students:
 def student_list():
-    print("The list of students: ")
+    if len(students)==0:
+        print("No student found")
+        return
+    print("The list of students: ")     
     for student_id in students:
-        student_name=students[student_id]['name']
-        print(student_id,student_name)
+        student_name= students[student_id]['name']
+        student_DoB= students[student_id]['DoB']
+        print (student_id, student_name, student_DoB)
 #function to show student marks for a given course:
 def show_marks():
+    course_list()
+    if len(courses)==0:
+        print("No course found")
+        return
     course_id=input("Enter course id: ")
     if course_id not in courses:
         print ("Course id not found")
+        return
+    if len(courses[course_id]['students'])==0:
+        print("No student found")
         return
     print("Student marks of" + course_id + ": ")
     for student_id in courses[course_id]['students']:
